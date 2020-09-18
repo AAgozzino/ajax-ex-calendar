10eeb8133bfe49f87c0f9e7afec85d009aad5f5e
Anna Agozzino <anna.agozzino91@gmail.com>
2020-09-18 16:44:05 +0200

    Render giorni festivi

--- js/main.js
+++ js/main.js
@@ -4,7 +4,9 @@ $(document).ready(function(){
   var momentDate = moment(startingDate);
   console.log(momentDate);
 
+  var dateYear = momentDate.format("YYYY");
   var dateMonth = momentDate.format("MMMM");
+  var dateMonthTwoDigits = momentDate.format("MM");
   console.log(dateMonth);
   var monthDays = momentDate.daysInMonth();
   console.log(monthDays);
@@ -16,14 +18,35 @@ $(document).ready(function(){
   // Render day for calendar
   for (var i = 1; i <= monthDays; i++) {
     var day = addZero(i);
+    var date = dateYear + "-" + dateMonthTwoDigits + "-" + day;
     //console.log(day);
     var context = {
       "day" : day,
-      "month" : dateMonth
+      "month" : dateMonth,
+      "date": date
     };
     var html= template(context);
     $("#days").append(html);
   }
+
+  $.ajax(
+    {
+      "url": "https://flynn.boolean.careers/exercises/api/holidays",
+      "data": {
+        "year" : 2018,
+        "month" : 0
+      },
+      "method" : "GET",
+      "success": function(data){
+        console.log(data.response);
+        isHoliday(data.response);
+      },
+      "error" : function(error){
+        alert("Errore!");
+      }
+    }
+  );
+
 });
 
 function addZero(number) {
@@ -31,4 +54,16 @@ function addZero(number) {
     return "0" + number
   }
   return number
+};
+
+function isHoliday(holidays) {
+  if (holidays.length > 0) {
+    for (var i = 0; i < holidays.length; i++) {
+      var holidayDate = holidays[i].date;
+      var holidayName = holidays[i].name;
+
+      $(".day[data-date='" + holidayDate + "']").addClass("holiday");
+      $(".day[data-date='" + holidayDate + "'] .holiday-name").text("- "+holidayName);
+    }
+  }
 }
