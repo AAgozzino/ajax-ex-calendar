da187f3b168444b9855785482f0415ba5997ba48
Anna Agozzino <anna.agozzino91@gmail.com>
2020-09-18 16:05:32 +0200

    Render giorni del mese

--- js/main.js
+++ js/main.js
@@ -1,3 +1,34 @@
 $(document).ready(function(){
+  // Starting date for calendar
+  var startingDate = "2018-01-01";
+  var momentDate = moment(startingDate);
+  console.log(momentDate);
 
+  var dateMonth = momentDate.format("MMMM");
+  console.log(dateMonth);
+  var monthDays = momentDate.daysInMonth();
+  console.log(monthDays);
+
+  // Template day for calendar
+  var source = $("#day-template").html();
+  var template = Handlebars.compile(source);
+
+  // Render day for calendar
+  for (var i = 1; i <= monthDays; i++) {
+    var day = addZero(i);
+    //console.log(day);
+    var context = {
+      "day" : day,
+      "month" : dateMonth
+    };
+    var html= template(context);
+    $("#days").append(html);
+  }
 });
+
+function addZero(number) {
+  if (number < 10) {
+    return "0" + number
+  }
+  return number
+}
