1c1f8100176e4d265b68759e76b43a574e6bef26
Anna Agozzino <anna.agozzino91@gmail.com>
2020-09-21 14:09:56 +0200

    Evento click next month

--- js/main.js
+++ js/main.js
@@ -2,68 +2,77 @@ $(document).ready(function(){
   // Starting date for calendar
   var startingDate = "2018-01-01";
   var momentDate = moment(startingDate);
-  console.log(momentDate);
+  //console.log(momentDate);
 
-  var dateYear = momentDate.format("YYYY");
-  var dateMonth = momentDate.format("MMMM");
-  var dateMonthTwoDigits = momentDate.format("MM");
-  console.log(dateMonth);
-  var monthDays = momentDate.daysInMonth();
-  console.log(monthDays);
+  printCalendar(momentDate);
+  printHolidays(momentDate);
 
+  // EVENT CLICK - show next month
+  $(".next").click(function(){
+    var newDate = momentDate.add(1, "M");
+    var newDateMonth = newDate.format("MM");
+    if (newDateMonth == 1) {
+      alert("Non è possibile visualizzare altre date")
+    } else {
+      printCalendar(newDate);
+      printHolidays(newDate);
+    }
+    //console.log(moment(newDate));
+  });
+});
+function printCalendar(date) {
   // Template day for calendar
+  $("#days").html("");
+  $("h1").text(date.format("MMMM YYYY"));
   var source = $("#day-template").html();
   var template = Handlebars.compile(source);
 
+  var dateClone = moment(date);
+  var monthDays = date.daysInMonth();
+  //console.log(monthDays);
   // Render day for calendar
   for (var i = 1; i <= monthDays; i++) {
-    var day = addZero(i);
-    var date = dateYear + "-" + dateMonthTwoDigits + "-" + day;
-    //console.log(day);
+
     var context = {
-      "day" : day,
-      "month" : dateMonth,
-      "date": date
+      "day" : i,
+      "month" : date.format("MMMM"),
+      "date": dateClone.format("YYYY-MM-DD")
     };
     var html= template(context);
     $("#days").append(html);
+
+    dateClone.add(1, "days")
   }
+}
+function printHolidays(date) {
+  var momentDateMonth = date.format("MM");
 
   $.ajax(
     {
       "url": "https://flynn.boolean.careers/exercises/api/holidays",
       "data": {
         "year" : 2018,
-        "month" : 0
+        "month" : momentDateMonth - 1
       },
       "method" : "GET",
       "success": function(data){
-        console.log(data.response);
-        isHoliday(data.response);
+        //console.log(data.response);
+        var response = data.response;
+        if (response.length > 0) {
+          for (var i = 0; i < response.length; i++) {
+            var holidayDate = response[i].date;
+            var holidayName = response[i].name;
+
+            var dataAttrDateSelector = $(".day[data-date='" + holidayDate + "']");
+            dataAttrDateSelector.addClass("holiday");
+            dataAttrDateSelector.children(".holiday-name").text("- "+holidayName);
+          }
+        }
+
       },
       "error" : function(error){
         alert("Errore!");
       }
     }
   );
-
-});
-
-function addZero(number) {
-  if (number < 10) {
-    return "0" + number
-  }
-  return number
 };
-
-function isHoliday(holidays) {
-  if (holidays.length > 0) {
-    for (var i = 0; i < holidays.length; i++) {
-      var holidayDate = holidays[i].date;
-      var holidayName = holidays[i].name;
-
-      $(".day[data-date='" + holidayDate + "']").addClass("holiday");
-      $(".day[data-date='" + holidayDate + "'] .holiday-name").text("- "+holidayName);
-    }
-  }
-}
